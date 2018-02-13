32039c2c2a4dd7eb6ad494fbf681c77fab089bdb
Roger Brogan <roger.brogan@avanade.com>
2018-02-13 16:46:02 -0600

    Added debouce.

--- After/src/Api/src/app/utils/validateUniqueEmail.ts
+++ After/src/Api/src/app/utils/validateUniqueEmail.ts
@@ -8,7 +8,7 @@ import { Observable } from 'rxjs/Observable';
 export class ValidateUniqueEmail {
     static createValidator(custService: CustomerService) {
         return (control: AbstractControl) => {
-            return custService.uniqueEmail(control.value).map(res => {
+            return custService.uniqueEmail(control.value).debounceTime(800).map(res => {
                 console.log(res);
                 return null; // null means validation is successful
             }).catch(ret => {
